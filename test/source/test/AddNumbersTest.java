6f2f5078cde161de3b454f6e3da4850b573a1e38
apresta <presta.alberto@libero.it>
2019-11-13 15:06:44 +0100

    initial commit

--- test/source/test/AddNumbersTest.java
+++ test/source/test/AddNumbersTest.java
@@ -1,4 +1,17 @@
-package test;
+package test.StringCalculator;
+
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddNumbersTest {
+
+
+    @Test
+    void emptyis0(){
+        StringCalculator strcalc = new StringCalculator("");
+        assertEquals(0,"");
+    }
+
 }
